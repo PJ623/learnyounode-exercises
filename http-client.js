f8050a0237b1deb59984928440eafc37f41a6111
PJ623 <jardiolin.paolo@gmail.com>
2018-01-18 17:11:56 -0800

    finished ex.7

---
+++ http-client.js
@@ -0,0 +1,12 @@
+// Exercise: "HTTP CLIENT"
+
+var http = require('http');
+
+http.get(process.argv[2], (res) => {
+    res.on('data', (data) => {
+        console.log(data.toString());
+    });
+    res.on('error', (err) => {
+        console.log(err);
+    });
+}).on('error', (err) => { console.log(err); });
